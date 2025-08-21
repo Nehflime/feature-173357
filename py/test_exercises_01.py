b575db2ad6fbf5018a8b2f6742c5ee69cdd440d2
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-08-21 17:47:30 -0600

    Update test_exercises_01.py

--- py/test_exercises_01.py
+++ py/test_exercises_01.py
@@ -13,19 +13,6 @@ class TestCodigoEstudiantes(unittest.TestCase):
             print("Valida que declaraste la variable nombre")
         if not isinstance(nombre, str):
             print("Verifica que la varibale nombre tenga asignada una cadena de caracteres")
-        assert isinstance(nombre, str)
-        self.assertIn(nombre, ['Castillo López Ana Karen', 'Cerrillo Domínguez César Alejandro',
-                               'Del Angel Rodríguez José Jesús', 'Durán Álvarez Edgar Aarón',
-                               'Elias Delgadillo Gerardo Gael', 'García Hernández Hugo Armando',
-                               'Guerrero González Diego Adolfo', 'Guerrero Torres José Emanuel',
-                               'Gutiérrez Hernández Rodolfo', 'López Cerda Aneth Alejandra',
-                               'López Cerda Jennifer Paola', 'Loredo Martínez Miguel Angel',
-                               'Martínez Hernández Cinthya Alejandra', 'Martínez Monreal Uriel',
-                               'Montejano Briano Uriel', 'Ochoa De León Guillermo',
-                               'Rodríguez Cardona Carlos Gabriel', 'Rodríguez Segura Sebastián',
-                               'Rodríguez Torres Sebastián', 'Romero García María Jaqueline',
-                               'Saldívar Juárez Pedro Antonio', 'Villanueva Infante Jorge Yahir',
-                               'Zavala Sanchez Christian Aaron'])
 
     def test_matricula(self):
         # Prueba para la variable matricula
@@ -35,15 +22,6 @@ class TestCodigoEstudiantes(unittest.TestCase):
             print("Valida que declaraste la variable matricula")
         if not isinstance(matricula, int):
             print("Verifica que la varibale nombre tenga asignada un entero")
-        assert isinstance(matricula, int)
-        self.assertIn(matricula, [173644, 179289, 175800,
-                                  178361, 177735, 175056,
-                                  177156, 176308, 179598,
-                                  173577, 175344, 178424,
-                                  172660, 178646, 177771,
-                                  176635, 177488, 175596,
-                                  179287, 173846, 174074,
-                                  175367, 171817])
 
     def test_sumar(self):
         # Pruebas para la función sumar
